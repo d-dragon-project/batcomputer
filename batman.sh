2d3158c3cfaa2f388d3ca6625b0ab990c61f9f50
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-01-18 06:26:08 +0800

    Update batman.sh

--- batman.sh
+++ batman.sh
@@ -6,7 +6,6 @@ echo "         Run after installing Kali Linux          "
 echo " This will reboot the Machine after Installation  "
 echo "##################################################"
 sleep 3
-clear
 apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
@@ -18,7 +17,6 @@ cp -f kali-laminaria-16x9.png /usr/share/backgrounds/kali/
 cp -f grub-4x3.png /boot/grub/themes/kali/
 cp -f grub-16x9.png /boot/grub/themes/kali/
 cp -f desktop-background /usr/share/images/desktop-base
-cp -f browser-home-page-banner.jpg /usr/share/kali-defaults/web/images
 apt-get install -y linux-image-$(uname -r|sed 's,[^-]*-[^-]*-,,') linux-headers-$(uname -r|sed 's,[^-]*-[^-]*-,,')
 dpkg -i dkms_3.0.12-1_all.deb
 dpkg -i b43-fwcutter_019-4_amd64.deb
