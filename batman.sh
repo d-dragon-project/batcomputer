5a5c9f7a9719ae2f8a2a4ee01c1e7a6cf54e5154
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-01-18 17:49:28 +0800

    Update batman.sh

--- batman.sh
+++ batman.sh
@@ -10,7 +10,7 @@ apt-get update
 apt-get upgrade -y
 apt-get dist-upgrade -y
 apt install apt-file -y
-apt-file update -y 
+apt-file update
 update-initramfs -u
 apt-get clean
 apt-get -y install ntp
