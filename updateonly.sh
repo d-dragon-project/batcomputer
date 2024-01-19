3fdea526dee4f89036f2ea59556a12051e85256c
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-01-20 01:09:49 +0800

    Update updateonly.sh

--- updateonly.sh
+++ updateonly.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+echo "=================================================="
+echo " Bat-Os is now performing the necessary updates   "
+echo "        UPDATE | UPGRADE | DIST-UPGRADE           "
+echo "=================================================="
+sleep 3
+cd /root/batcomputer
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt install apt-file -y
+apt-file update
+update-initramfs -u
+apt-get clean
