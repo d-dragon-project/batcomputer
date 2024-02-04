e2d68d9363bc165d4430687f0cc3d650c0d6d55f
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-02-04 21:40:21 +0800

    Create parrot.sh

---
+++ parrot.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+clear
+echo "========================================================"
+echo "   Bat-OS is now preparing for the necessary updates    "
+echo " Open the README file and follow necessary instructions "
+echo "========================================================"
+sleep 3
+pkill -f firefox
+cd /etc/ssh/
+dpkg-reconfigure openssh-server
+cd /usr/share/backgrounds/
+mv -f default default-
+mv -f default.jpg default-.jpg
+mv -f kali-logo.svg kali-logo.svg-
+cd /root/batcomputer/
+cp -f kali-logo.svg /usr/share/kali-defaults/web/images/
+cp -f browser-home-page-banner.jpg /usr/share/kali-defaults/web/images/
+cp batman.png  /usr/share/images
+cp bat-icon.png /usr/share/images
+cp bat-icon.jpg /usr/share/images
+cp bat-icon.gif /usr/share/images
+cp bat-icon2.png /usr/share/images
+cp bat-os-icon.png /usr/share/images
+cp -f desktop-grub.png /usr/share/images/desktop-base/
+cp -f kali-laminaria-16x9.png /usr/share/backgrounds/kali/
+cp -f kali-laminaria-16x9.png /usr/share/backgrounds/kali-16x9/
+cp -f grub-4x3.png /boot/grub/themes/kali/
+cp -f grub-16x9.png /boot/grub/themes/kali/
+cp -f desktop-background /usr/share/images/desktop-base
+cp -f plymouth_background_future.png /usr/share/plymouth/themes/futureprototype/
+cd /usr/share/plymouth/themes/futureprototype/
+rm -f logo.png
+rm -f debian.png
+plymouth-set-default-theme -R futureprototype
+plymouth-set-default-theme
+clear 
+echo "=================================================="
+echo " Bat-Os is now performing the necessary updates   "
+echo "        UPDATE | UPGRADE | DIST-UPGRADE           "
+echo "=================================================="
+sleep 3
+cat /etc/os-release
+cd /root/batcomputer
+apt-get update
+apt-get upgrade -y
+apt-get dist-upgrade -y
+apt install apt-file -y
+apt-file update
+update-initramfs -u
+apt-get clean
+clear
+echo "=================================================="
+echo "   Bat-Os is now installing necessary softwares   "    
+echo "          NTP | GRUB CUSTOMIZER | GIMP            "
+echo "=================================================="
+sleep 3
+apt-get -y install ntpdate
+apt-get -y install ntp
+ntpdate -u "time.nist.gov"
+sudo service ntp restart
+apt-get -y install grub-customizer
+apt-get -y install gimp
+apt-get -y install gdebi
+apt-get -y install tilix
+apt-get -y install htop
+apt-get -y install default-jdk
+clear
+echo "=================================================="
+echo "          Bat-OS has completed installation       "     
+echo "           SYSTEM WILL REBOOT IN A MOMENTS        "  
+echo "=================================================="
+apt auto-remove -y
+sleep 5
+reboot
