135e1991f96140ca40c85219327c30875e3a36c3
patukista <155352616+d-dragon-project@users.noreply.github.com>
2024-02-28 23:49:39 +0000

    Update parrot.sh

--- parrot.sh
+++ parrot.sh
@@ -63,5 +63,6 @@ echo "          Bat-OS has completed installation       "
 echo "           SYSTEM WILL REBOOT IN A MOMENTS        "  
 echo "=================================================="
 apt auto-remove -y
+cp myscript.service /etc/systemd/system/
 sleep 5
 reboot
